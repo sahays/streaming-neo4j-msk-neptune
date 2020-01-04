97996cd3f336e8bbf17b9603f69719b67ef3b767
Sanjeet Sahay <sanjeet@earthrise.local>
2020-01-04 15:09:03 -0800

    logic to stream to neptune in progress

--- kafka-consumer-app/config.js
+++ kafka-consumer-app/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   topic: "Neo4jPersonTopic",
   brokers:
-    "b-1.neo4j-kafka.wqfvy4.c3.kafka.us-west-2.amazonaws.com:9094,b-2.neo4j-kafka.wqfvy4.c3.kafka.us-west-2.amazonaws.com:9094"
+    "b-1.neo4j-kafka.wqfvy4.c3.kafka.us-west-2.amazonaws.com:9094,b-2.neo4j-kafka.wqfvy4.c3.kafka.us-west-2.amazonaws.com:9094",
+  neptune:
+    "neptunedbcluster.cluster-cwtrcbvtfrn8.us-west-2.neptune.amazonaws.com"
 };
--- kafka-consumer-app/index.js
+++ kafka-consumer-app/index.js
@@ -1,5 +1,13 @@
 const { Kafka } = require("kafkajs");
+const gremlin = require("gremlin");
 const config = require("./config");
+
+const traversal = gremlin.process.AnonymousTraversalSource.traversal;
+const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
+
+const g = traversal().withRemote(
+  new DriverRemoteConnection("http://" + config.neptune + "/gremlin")
+);
 
 const brokers = config.brokers.split(",");
 console.log("connecting to", brokers);
@@ -19,11 +27,38 @@ const run = async () => {
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message.value.toString()
-      });
+      const result = JSON.parse(message.value.toString());
+      if (result.meta && result.payload) {
+        const payload = result.payload;
+        if (payload && payload.type === "node") {
+          const id = payload.id;
+          if (payload.before && payload.after) {
+            // edited
+          } else if (payload.after) {
+            // inserted
+            payload.after.labels.map(label => {
+              const inserted = payload.after.properties;
+              g.V()
+                .addV(label)
+                .properties(inserted)
+                .next();
+            });
+          } else if (payload.before) {
+            // deleted
+          }
+        } else if (payload && payload.type === "relationship") {
+          const type = payload.label; // e.g. KNOWS or ACTED_IN
+          const start = payload.start;
+          const end = payload.end;
+          if (payload.before && payload.after) {
+            // edited
+          } else if (payload.after) {
+            // created
+          } else if (payload.before) {
+            // deleted
+          }
+        }
+      }
     }
   });
 };
