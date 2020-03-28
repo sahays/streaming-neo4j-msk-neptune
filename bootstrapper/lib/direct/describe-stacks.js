87a7918606b3bc8661bff448c50d34934c165b3e
Sanjeet Sahay <sanjeet@earthrise.local>
2020-03-28 09:21:37 -0700

    describe-stacks

--- bootstrapper/lib/direct/describe-stacks.js
+++ bootstrapper/lib/direct/describe-stacks.js
@@ -1,20 +1,24 @@
 const AWS = require("aws-sdk");
 
 const describeStack = async () => {
-  const cfn = new AWS.CloudFormation({
+  const cfnArgs = {
     apiVersion: "2010-05-15",
     region: process.env.AWS_REGION
-  });
-  const neptuneStackJson = await cfn
-    .describeStacks({ StackName: "streaming-blog-neptune-stack" })
-    .promise();
-  const mskStackJson = await cfn
-    .describeStacks({ StackName: "streaming-blog-msk-stack" })
-    .promise();
-  console.log(neptuneStackJson);
-  console.log(mskStackJson);
+  };
+  console.log(cfnArgs);
+  try {
+    const cfn = new AWS.CloudFormation(cfnArgs);
+    const neptuneStackJson = await cfn
+      .describeStacks({ StackName: "streaming-blog-neptune-stack" })
+      .promise();
+    const mskStackJson = await cfn
+      .describeStacks({ StackName: "streaming-blog-msk-stack" })
+      .promise();
+    console.log(neptuneStackJson);
+    console.log(mskStackJson);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-console.log("describing stacks...");
 describeStack();
-console.log("done");
