fde56d4bbe5752fd69de8f19344a42f2e23c4fa5
Sanjeet Sahay <sanjeet@earthrise.local>
2020-04-03 16:11:31 -0700

    startup

--- docker-files/neo4j/run-script.sh
+++ docker-files/neo4j/run-script.sh
@@ -4,6 +4,5 @@
     wget --directory-prefix ${NEO4J_HOME}/plugins/ https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.2/apoc-4.0.0.2-all.jar 
     wget --directory-prefix ${NEO4J_HOME}/plugins/ https://github.com/neo4j-contrib/neo4j-streams/releases/download/4.0.0/neo4j-streams-4.0.0.jar
     cp ${JAVA_HOME}/lib/security/cacerts ${NEO4J_HOME}/temp/kafka.client.truststore.jks
-    neo4j-admin set-initial-password "${NEO4J_PWD}"
     neo4j console
 } 2>&1 | tee run-script.log
--- startup.sh
+++ startup.sh
@@ -2,8 +2,10 @@
 {
     docker-compose -f 01-docker-compose.yml down
     docker-compose -f 02-docker-compose.yml down
-    docker-compose -f 01-docker-compose.yml up --remove-orphans --build
+    docker-compose -f 01-docker-compose.yml build --no-cache
+    docker-compose -f 01-docker-compose.yml up
     export DOCKER_VOL_PATH=`docker volume inspect --format '{{ .Mountpoint }}' streaming-neo4j-msk-neptune_shared-folder`
     . /$DOCKER_VOL_PATH/setup-env.sh
-    docker-compose -f 02-docker-compose.yml up -d --remove-orphans --build 
+    docker-compose -f 02-docker-compose.yml build --no-cache
+    docker-compose -f 02-docker-compose.yml up
 } 2>&1 | tee startup.log
