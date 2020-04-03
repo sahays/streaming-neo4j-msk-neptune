9ddba394ec3dd90051d344be631a6635a54c72f3
Sanjeet Sahay <sanjeet@earthrise.local>
2020-04-03 09:38:38 -0700

    shared-volume

--- docker-files/after-deploy/run-script.sh
+++ docker-files/after-deploy/run-script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 {
-    rm -rf /streaming-neo4j-msk-neptune/bootstrapper
-    git clone https://github.com/sahays/streaming-neo4j-msk-neptune.git
-    cd /streaming-neo4j-msk-neptune/bootstrapper
-    npm install
-    cd /
-    node /streaming-neo4j-msk-neptune/bootstrapper/lib/direct/describe-stacks.js
+    rm -rf /streaming-neo4j-msk-neptune/bootstrapper && \
+    git clone https://github.com/sahays/streaming-neo4j-msk-neptune.git && \
+    cd /streaming-neo4j-msk-neptune/bootstrapper && \
+    npm install && \
+    cd / && \
+    node /streaming-neo4j-msk-neptune/bootstrapper/lib/direct/describe-stacks.js && \
     node /streaming-neo4j-msk-neptune/bootstrapper/lib/direct/export-msk-neptune-info.js
 } 2>&1 | tee run-script.log
