28aa845de98115a85d480172e85ed6224d19fec2
Sanjeet Sahay <sanjeet@earthrise.local>
2020-04-03 15:18:58 -0700

    startup

--- docker-files/neo4j/run-script.sh
+++ docker-files/neo4j/run-script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 {
-    cp ${JAVA_HOME}/lib/security/cacerts /tmp/kafka.client.truststore.jks
+    mkdir -p temp
+    cp ${JAVA_HOME}/lib/security/cacerts ./temp/kafka.client.truststore.jks
     neo4j console
 } 2>&1 | tee run-script.log
