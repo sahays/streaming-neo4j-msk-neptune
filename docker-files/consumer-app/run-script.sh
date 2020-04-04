55d617cab08d5ef6b433fd44100702371e24ed37
Sanjeet Sahay <sanjeet@earthrise.local>
2020-04-04 12:08:32 -0700

    consumer

--- docker-files/consumer-app/run-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-{
-    rm -rf streaming-neo4j-msk-neptune/
-    git clone https://github.com/sahays/streaming-neo4j-msk-neptune.git 
-    cd /streaming-neo4j-msk-neptune/msk-consumer-app
-    npm install
-    node index.js
-} 2>&1 | tee run-script.log
