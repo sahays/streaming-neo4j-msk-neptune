57d2c3cce8a64725b46e98e720e0d33f89ca1ba6
Sanjeet Sahay <sanjeet@earthrise.local>
2020-04-03 18:51:03 -0700

    startup

--- docker-files/neo4j/run-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-{
-    mkdir -p temp
-    wget --directory-prefix ${NEO4J_HOME}/plugins/ https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.0.0.2/apoc-4.0.0.2-all.jar 
-    wget --directory-prefix ${NEO4J_HOME}/plugins/ https://github.com/neo4j-contrib/neo4j-streams/releases/download/4.0.0/neo4j-streams-4.0.0.jar
-    cp ${JAVA_HOME}/lib/security/cacerts ${NEO4J_HOME}/temp/kafka.client.truststore.jks
-} 2>&1 | tee run-script.log
