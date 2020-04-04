2b061cfeb8f16036c93144caaa425512ebe9ae77
Sanjeet Sahay <sanjeet@earthrise.local>
2020-04-03 19:51:36 -0700

    startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
-docker-compose -f 01-docker-compose.yml down && \
 docker-compose -f 02-docker-compose.yml down && \
+docker-compose -f 01-docker-compose.yml down && \
 docker-compose -f 01-docker-compose.yml up --build --remove-orphans && \
 export DOCKER_VOL_PATH=`docker volume inspect --format '{{ .Mountpoint }}' streaming-neo4j-msk-neptune_shared-folder` && \
 . /$DOCKER_VOL_PATH/setup-env.sh && \
